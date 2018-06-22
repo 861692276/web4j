fadabf055718f10f3ca3c0ec67afb3b85895c7a0
Bingo <Bingo@Alading>
2018-06-22 14:30:49 +0800

    ok

---
+++ web4j.base/src/main/java/org/web4j/base/App.java
@@ -0,0 +1,13 @@
+package org.web4j.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ web4j.wiki/src/main/java/org/web4j/wiki/App.java
@@ -0,0 +1,13 @@
+package org.web4j.wiki;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
